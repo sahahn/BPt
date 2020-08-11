a1dc45c3c6cac3eb2c7ac9acefa9d59ac970281b
sahahn <sahahn@uvm.edu>
2020-08-11 10:53:30 -0400

    Still allow ABCD_ML

--- BPt/__init__.py
+++ BPt/__init__.py
@@ -1,4 +1,5 @@
 from .main.BPt_ML import BPt_ML, Load
+from .main.BPt_ML import BPt_ML as ABCD_ML
 from .main.Params_Classes import (Loader, Imputer, Scaler, Transformer,
                                   Feat_Selector, Model, Ensemble,
                                   Param_Search, Feat_Importance,
@@ -10,7 +11,7 @@ from .main.Input_Tools import (Select, Duplicate, Pipe, Value_Subset)
 
 __author__ = "sahahn"
 __version__ = "1.1"
-__all__ = ["BPt_ML", "Load", "Loader",
+__all__ = ["BPt_ML", "ABCD_ML", "Load", "Loader",
            "Imputer", "Scaler", "Transformer",
            "Feat_Selector", "Model",
            "Ensemble", "Param_Search", "Feat_Importance",

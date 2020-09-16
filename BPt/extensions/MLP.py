913be087b56399d680706bbadcf5d060dd3c62c2
sahahn <sahahn@uvm.edu>
2020-09-16 15:25:18 -0400

    Change name from wrapper to BPt

--- BPt/extensions/MLP.py
+++ BPt/extensions/MLP.py
@@ -1,6 +1,7 @@
 from sklearn.neural_network import MLPRegressor, MLPClassifier
 import numpy as np
 
+
 class MLPRegressor_Wrapper(MLPRegressor):
 
     def _fit(self, X, y, incremental=False):
@@ -8,10 +9,10 @@ class MLPRegressor_Wrapper(MLPRegressor):
         self.hidden_layer_sizes = np.array(self.hidden_layer_sizes).astype(int)
         super()._fit(X, y, incremental=False)
 
+
 class MLPClassifier_Wrapper(MLPClassifier):
 
     def _fit(self, X, y, incremental=False):
 
         self.hidden_layer_sizes = np.array(self.hidden_layer_sizes).astype(int)
         super()._fit(X, y, incremental=False)
-

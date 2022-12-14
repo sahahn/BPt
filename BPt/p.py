68dbbbeeaaafe12ed2d908bdf435b65b26c38a45
Sage Hahn <sahforever82@gmail.com>
2022-12-14 16:23:16 -0500

    Revert "Revert "Update p.py""

--- BPt/p.py
+++ BPt/p.py
@@ -1,3 +1,4 @@
 from .default.params.Params import (Choice, TransitionChoice,
                                     Array, Scalar, Log, Tuple,
                                     Instrumentation, Dict)
+
